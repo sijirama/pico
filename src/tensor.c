6bb19b893a6868d4595bc520988086e4d47b1f55
sijirama <gbemilesanmi@gmail.com>
2026-01-01 19:38:32 +0100

    init pico

--- src/tensor.c
+++ src/tensor.c
@@ -2,38 +2,53 @@
 
 #include <stdio.h>
 
-// cpu_ops declarations
-void tensor_matmul_cpu(void* x);
-void tensor_mean_cpu();
-
-// gpu_ops declarations
-void tensor_matmul_gpu(void* x);
-void tensor_mean_gpu();
+// cpu_ops prototypes
+void tensor_matmul_cpu(struct Tensor* x, struct Tensor* y);
+void tensor_add_cpu(struct Tensor* x, struct Tensor* y);
+void tensor_mean_cpu(struct Tensor* x);
 
 // ops tables
-static const struct TensorOps tensor_ops_cpu = {tensor_matmul_cpu, tensor_mean_cpu};
-static const struct TensorOps tensor_ops_gpu = {tensor_matmul_gpu, tensor_mean_gpu};
+static const struct TensorOps tensor_ops_cpu = {tensor_matmul_cpu, tensor_add_cpu, tensor_mean_cpu};
+static const struct TensorOps tensor_ops_gpu = {tensor_matmul_cpu, tensor_add_cpu,
+                                                tensor_mean_cpu};  // use cpu functions for now
 
+// helper functions
+int tensor_ndim(struct Tensor* t) {
+    return t->ndim;
+}
+int* tensor_shape(struct Tensor* t) {
+    return t->shape;
+}
+float tensor_get_nd(struct Tensor* t, int* coords) {
+    int final_index = 0;
+    for(int i = 0; i < t->ndim; i++) {
+        final_index += coords[i] * t->strides[i];
+    }
+    return t->data[final_index];
+}
+void tensor_update_strides(struct Tensor* t) {
+    int current_stride = 1;
+    for(int i = t->ndim - 1; i >= 0; i--) {
+        t->strides[i] = current_stride;
+        current_stride *= t->shape[i];
+    }
+}
+
+// move to device
 void tensor_to_cpu(struct Tensor* t) {
     t->ops = (struct TensorOps*)&tensor_ops_cpu;
 }
-
 void tensor_to_gpu(struct Tensor* t) {
     t->ops = (struct TensorOps*)&tensor_ops_gpu;
 }
 
 // CPU OPS
-void tensor_matmul_cpu(void* x) {
+void tensor_matmul_cpu(struct Tensor* x, struct Tensor* y) {
     printf("CPU_MATMUL\n");
 };
-void tensor_mean_cpu() {
+void tensor_add_cpu(struct Tensor* x, struct Tensor* y) {
+    printf("CPU_MATMUL\n");
+};
+void tensor_mean_cpu(struct Tensor* x) {
     printf("CPU_CONCAT\n");
 };
-
-// GPU OPS
-void tensor_matmul_gpu(void* x) {
-    printf("GPU_MATMUL\n");
-}
-void tensor_mean_gpu() {
-    printf("GPU_MEAN\n");
-}
--- src/tensor.h
+++ src/tensor.h
@@ -1,19 +1,24 @@
 #pragma once
-
 #define MAX_DIMS 8
-
-struct TensorOps {
-    void (*matmul)(void* x);
-    void (*mean)();
-};
 
 struct Tensor {
     struct TensorOps* ops;  // operations for the tensor
     float* data;
     int shape[MAX_DIMS];
-    int strides[MAX_DIMS];  // The "jump" needed in memory
+    int strides[MAX_DIMS];
     int ndim;
+};
+
+struct TensorOps {
+    void (*matmul)(struct Tensor* x, struct Tensor* y);
+    void (*add)(struct Tensor* x, struct Tensor* y);
+    void (*mean)(struct Tensor* x);
 };
 
 void tensor_to_cpu(struct Tensor* t);
 void tensor_to_gpu(struct Tensor* t);
+
+int tensor_ndim(struct Tensor* t);
+int* tensor_shape(struct Tensor* t);
+float tensor_get_nd(struct Tensor* t, int* coords);
+void tensor_update_strides(struct Tensor* t);
