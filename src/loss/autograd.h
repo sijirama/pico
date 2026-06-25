8ac23afb77596830c798fb814a5bba7380eeeaa5
sijirama <gbemilesanmi@gmail.com>
2026-06-25 05:46:44 +0100

    sum reduction for the mse

--- src/loss/autograd.h
+++ src/loss/autograd.h
@@ -1,17 +1,12 @@
-
-#pragma once
-
 /*
- *
  * for better details onbackward functions check out ../autograd.h
- *
  */
 
+#pragma once
 #include "tensor.h"
 
-// static inline void pico_mse_loss_backward(struct PicoTensor * self){}
 
-static inline void pico_mse_loss_backward(struct PicoTensor* self) {
+static inline void pico_mse_loss_mean_backward(struct PicoTensor* self) {
     struct PicoTensor* prediction = self->parents[0];
     struct PicoTensor* actuals = self->parents[1];
 
@@ -22,6 +17,23 @@ static inline void pico_mse_loss_backward(struct PicoTensor* self) {
         
         // local sensitivity: d(loss)/d(pred_i) = (2/N) * (pred_i - actual_i)
         float local = (2.0f / N) * (prediction->data[i] - actuals->data[i]);
+
+        prediction->grad[i] += local * upstream;
+        actuals->grad[i] += -local * upstream;
+    }
+}
+
+static inline void pico_mse_loss_sum_backward(struct PicoTensor* self) {
+    struct PicoTensor* prediction = self->parents[0];
+    struct PicoTensor* actuals = self->parents[1];
+
+    float upstream = self->grad[0];  // the loss is a single scalar
+    int64_t N = prediction->numel;
+
+    for(int64_t i = 0; i < N; i++) {
+        
+        // local sensitivity: d(loss)/d(pred_i) = (2/N) * (pred_i - actual_i)
+        float local = (2.0f) * (prediction->data[i] - actuals->data[i]);
 
         prediction->grad[i] += local * upstream;
         actuals->grad[i] += -local * upstream;
--- src/loss/loss.h
+++ src/loss/loss.h
@@ -2,12 +2,21 @@
 #pragma once
 #include "tensor.h"
 
+
+// ==================== MSE
+
 enum PicoMSEReductionType { MEAN, SUM, NONE };
 
 struct PicoMSELoss {
     enum PicoMSEReductionType reduction;
 };
 
-
 struct PicoMSELoss* pico_mse_loss_init(struct Arena * arena, enum PicoMSEReductionType reduction);
 struct PicoTensor* pico_mse_loss(struct PicoMSELoss * mse, struct PicoTensor* predictions, struct PicoTensor* actuals);
+
+
+// ==================== Nesterov accelerated gradient (NAG)
+// ==================== AdaGrad
+// ==================== RMSProp
+// ==================== ADAM
+// ==================== Muon
