af1fca3f83a7445f0188bf7926685ab2228f61a5
sijirama <gbemilesanmi@gmail.com>
2026-06-25 04:35:48 +0100

    checkpoint for tonight

--- src/loss/autograd.h
+++ src/loss/autograd.h
@@ -1,12 +1,29 @@
 
-#pragma once 
+#pragma once
 
 /*
-*
-* for better details onbackward functions check out ../autograd.h
-*
-*/
+ *
+ * for better details onbackward functions check out ../autograd.h
+ *
+ */
 
 #include "tensor.h"
 
-static inline void pico_mse_loss_backward(struct PicoTensor * self){}
+// static inline void pico_mse_loss_backward(struct PicoTensor * self){}
+
+static inline void pico_mse_loss_backward(struct PicoTensor* self) {
+    struct PicoTensor* prediction = self->parents[0];
+    struct PicoTensor* actuals = self->parents[1];
+
+    float upstream = self->grad[0];  // the loss is a single scalar
+    int64_t N = prediction->numel;
+
+    for(int64_t i = 0; i < N; i++) {
+        
+        // local sensitivity: d(loss)/d(pred_i) = (2/N) * (pred_i - actual_i)
+        float local = (2.0f / N) * (prediction->data[i] - actuals->data[i]);
+
+        prediction->grad[i] += local * upstream;
+        actuals->grad[i] += -local * upstream;
+    }
+}
