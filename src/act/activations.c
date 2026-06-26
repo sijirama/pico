d0ac4fe1b7d7de17db4faced6b6647440f0dbde9
sijirama <gbemilesanmi@gmail.com>
2026-06-26 10:35:47 +0100

    sigmoid and tanh activation function

--- src/act/activations.c
+++ src/act/activations.c
@@ -1,6 +1,6 @@
-
+#include <math.h>
+#define _USE_MATH_DEFINES
 #include "activations.h"
-
 #include "arena.h"
 #include "autograd.h"
 #include "tensor.h"
@@ -21,6 +21,46 @@ struct PicoTensor* pico_relu(struct PicoTensor* x) {
     out->parents[0] = x;
     out->num_parents = 1;
     out->_backward = pico_relu_backward;
+
+    return out;
+}
+
+struct PicoTensor* pico_sigmoid(struct PicoTensor* x) {
+    struct Arena* arena = arena_ctx_current();
+    if(arena == NULL) {
+        fprintf(stderr, "[Pico] Error: No current arena in context!\n");
+        return NULL;
+    }
+    struct PicoTensor* out = pico_create_tensor(arena, x->shape, x->ndim);
+
+    for(int i = 0; i < x->numel; i++) {
+        out->data[i] = sigmoid(out->data[i]);
+    }
+
+    out->parents = arena_alloc(arena, sizeof(struct PicoTensor*));
+    out->parents[0] = x;
+    out->num_parents = 1;
+    out->_backward = pico_sigmoid_backward;
+
+    return out;
+}
+
+struct PicoTensor* pico_tanh(struct PicoTensor* x) {
+    struct Arena* arena = arena_ctx_current();
+    if(arena == NULL) {
+        fprintf(stderr, "[Pico] Error: No current arena in context!\n");
+        return NULL;
+    }
+    struct PicoTensor* out = pico_create_tensor(arena, x->shape, x->ndim);
+
+    for(int i = 0; i < x->numel; i++) {
+        out->data[i] = tanh(out->data[i]);
+    }
+
+    out->parents = arena_alloc(arena, sizeof(struct PicoTensor*));
+    out->parents[0] = x;
+    out->num_parents = 1;
+    out->_backward = pico_tanh_backward;
 
     return out;
 }
--- src/act/autograd.h
+++ src/act/autograd.h
@@ -1,6 +1,9 @@
 
 #pragma once
 
+#include <math.h>
+
+#include "activations.h"
 #include "tensor.h"
 
 static inline void pico_relu_backward(struct PicoTensor* self) {
@@ -10,5 +13,25 @@ static inline void pico_relu_backward(struct PicoTensor* self) {
 
     for(int64_t i = 0; i < N; i++) {
         parent->grad[i] += self->grad[i] * (self->data[i] > 0);
+    }
+}
+
+static inline void pico_sigmoid_backward(struct PicoTensor* self) {
+    struct PicoTensor* parent = self->parents[0];
+
+    int64_t N = parent->numel;
+
+    for(int64_t i = 0; i < N; i++) {
+        parent->grad[i] += self->grad[i] * (sigmoid(self->data[i]) * (1 - sigmoid(self->data[i])));
+    }
+}
+
+static inline void pico_tanh_backward(struct PicoTensor* self) {
+    struct PicoTensor* parent = self->parents[0];
+
+    int64_t N = parent->numel;
+
+    for(int64_t i = 0; i < N; i++) {
+        parent->grad[i] += self->grad[i] * (1 - powf(tanh(self->data[i]), 2.0f));
     }
 }
