bc532e0e7da3c110c85f22c2f773abbdbb8ed012
sijirama <gbemilesanmi@gmail.com>
2026-06-26 10:35:47 +0100

    sigmoid and tanh activation function

--- src/act/activations.h
+++ src/act/activations.h
@@ -1,7 +1,14 @@
 
 
-
 #pragma once
+#include <math.h>
+
 #include "tensor.h"
 
-struct PicoTensor * pico_relu(struct PicoTensor * x);
+static inline float sigmoid(float x) {
+    return exp(x) / exp(x) + 1;
+}
+
+struct PicoTensor* pico_relu(struct PicoTensor* x);
+struct PicoTensor* pico_sigmoid(struct PicoTensor* x);
+struct PicoTensor* pico_tanh(struct PicoTensor* x);
