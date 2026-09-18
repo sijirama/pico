936ae4a79835ac35ce785eeb85b6682c1882bcb6
sijirama <gbemilesanmi@gmail.com>
2026-09-18 10:16:01 +0100

    cuda gemm corrections

--- src/kernels/cuda/gemm/double_buffer.cu
+++ src/kernels/cuda/gemm/double_buffer.cu
@@ -1,29 +1,12 @@
 #include "gemm.cuh"
 
 #include <cooperative_groups.h>
-#include <cuda/pipeline>
+#include <cuda_pipeline.h>
 #include <cuda_runtime.h>
 #define TILE_WIDTH 16
 
-namespace cg = cooperative_groups;
-
-__global__ void double_buffer(
-    const float *A,
-    const float *B,
-    float *C,
-    int M,
-    int N,
-    int K) {
-
-    auto block = cg::this_thread_block();
-
-    __shared__ cuda::
-        pipeline_shared_state<cuda::thread_scope_block, 2>
-            shared_state;
-
-    // Create the pipeline handler
-    cuda::pipeline pipeline =
-        cuda::make_pipeline(block, &shared_state);
+__global__ void
+double_buffer(const float *A, const float *B, float *C, int M, int N, int K) {
 
     int row = threadIdx.y;
     int col = threadIdx.x;
@@ -35,21 +18,18 @@ __global__ void double_buffer(
     __shared__ float B_s[2][TILE_WIDTH][TILE_WIDTH];
 
     // prologue
-    pipeline.producer_acquire();
 
-    cuda::memcpy_async(
+    __pipeline_memcpy_async(
         &A_s[0][row][col],
         &A[thready * K + (0 * TILE_WIDTH + col)],
-        sizeof(float),
-        pipeline);
+        sizeof(float));
 
-    cuda::memcpy_async(
+    __pipeline_memcpy_async(
         &B_s[0][row][col],
         &B[(0 * TILE_WIDTH + row) * N + threadx],
-        sizeof(float),
-        pipeline);
+        sizeof(float));
 
-    pipeline.producer_commit();
+    __pipeline_commit();
 
     float sum = 0.0f;
     for(int phase = 0; phase < K / TILE_WIDTH; phase++) {
@@ -59,51 +39,43 @@ __global__ void double_buffer(
         int nextStage = nextPhase % 2;
 
         if(nextPhase < K / TILE_WIDTH) {
-            pipeline.producer_acquire();
 
-            cuda::memcpy_async(
+            __pipeline_memcpy_async(
                 &A_s[nextStage][row][col],
-                &A[thready * K +
-                   (nextPhase * TILE_WIDTH + col)],
-                sizeof(float),
-                pipeline);
+                &A[thready * K + (nextPhase * TILE_WIDTH + col)],
+                sizeof(float));
 
-            cuda::memcpy_async(
+            __pipeline_memcpy_async(
                 &B_s[nextStage][row][col],
-                &B[(nextPhase * TILE_WIDTH + row) * N +
-                   threadx],
-                sizeof(float),
-                pipeline);
+                &B[(nextPhase * TILE_WIDTH + row) * N + threadx],
+                sizeof(float));
 
-            pipeline.producer_commit();
+            __pipeline_commit();
         }
 
-        pipeline.consumer_wait();
+        // "allow N newest groups to remain unfinished"
+        if(nextPhase < K / TILE_WIDTH) {
+            __pipeline_wait_prior(1);
+        } else {
+            __pipeline_wait_prior(0);
+        }
+
         __syncthreads();
 
         for(int k = 0; k < TILE_WIDTH; k++) {
-            sum += A_s[currentStage][row][k] *
-                   B_s[currentStage][k][col];
+            sum += A_s[currentStage][row][k] * B_s[currentStage][k][col];
         }
 
         __syncthreads();
-        pipeline.consumer_release();
     }
 
     C[thready * N + threadx] = sum;
 }
 
 void cuda_gemm_double_buffered(
-    const float *A,
-    const float *B,
-    float *C,
-    int M,
-    int N,
-    int K) {
+    const float *A, const float *B, float *C, int M, int N, int K) {
 
     dim3 block(16, 16);
-    dim3 grid(
-        (N + block.x - 1) / block.x,
-        (M + block.y - 1) / block.y);
+    dim3 grid((N + block.x - 1) / block.x, (M + block.y - 1) / block.y);
     double_buffer<<<grid, block>>>(A, B, C, M, N, K);
 }
